3f4672a2f3e8704b740f39571dd99b2b8d17ae57
avula koti <koteshpatel6@gmail.com>
2020-02-11 19:10:15 +0530

    Initial Commit

---
+++ QuantsAppTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuantsAppTask
+//
+//  Created by avula koti on 11/02/20.
+//  Copyright © 2020 avula koti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
